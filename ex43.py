ad8bb745772de11c458b01b3f65f78185f421f92
Leeoku <leeoku@gmail.com>
2018-04-02 18:53:35 -0400

    First attempt map func

--- ex43.py
+++ ex43.py
@@ -1,51 +1,66 @@
- class Scene(obj ect) :
+from sys import exit
+from random import randint
 
-def enter(self) :
-    pass
+class Scene(object) :
 
-class Engine(obj ect) :
+    def enter(self) :
+        print("You are exiting the game")
+        exit(0)
 
-def __init__(self, scene_map) :
-    pass
+class Engine(object) :
 
-def play(self) :
-    pass
+    def __init__(self, scene_map) :
+        pass
+
+    def play(self) :
+        pass
 
 class Death(Scene) :
 
-def enter(self) :
-    pass
+    def enter(self) :
+        print(f"You are unsuccessful! Try again")
+        exit(0)
 
 class CentralCorridor(Scene) :
 
-def enter(self) :
-    pass
+    def enter(self) :
+        print("You hear a crash on your ship and the alarms go off. A large Gorthon army appears!")
+    def joke(self):
+        print("What joke should you ask the Gorthon? ")
 
 class LaserWeaponArmory(Scene) :
-def enter(self) :
-    pass
+    def enter(self) :
+        pass
 
-30 class TheBridge(Scene) :
-def enter(self) :
-    pass
+class TheBridge(Scene) :
+    def enter(self) :
+        pass
 
-35 class EscapePod(Scene) :
+class EscapePod(Scene) :
 
-37 def enter(self) :
-    pass
+    def enter(self) :
+        pass
+    
+class Map(object) :
+    scenes = {
+        'death': Death(),
+        "central_corridor": CentralCorridor(),
+        'laser_armory': LaserWeaponArmory(),
+        'bridge': TheBridge(),
+        'escape_pod': EscapePod()
+    }
+    def __init__(self, start_scene) :
+        self.start_scene = start_scene
+        print(start_scene)
 
-class Map(obj ect) :
+    def next_scene(self, scene_name) :
+        newroom = Map.scenes.get(scene_name)
+        return newroom
 
-43 def __init__(self, start_scene) :
-    pass
-
-46 def next_scene(self, scene_name) :
-    pass
-
-def opening_scene(self) :
-    pass
+    def opening_scene(self) :
+        return self.next_scene(scene_name)
 
 
-a_map = Map(' central_corridor' )
-a_game = Engine(a_map)
-a_game. play()
+a_map = Map('central_corridor' )
+#a_game = Engine(a_map)
+#a_game.play()
